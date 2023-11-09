7a47679774f254c07c8df653c4610998c17b7d54
dinoxido <130578197+dinoxido@users.noreply.github.com>
2023-11-09 08:55:59 -0300

    Add files via upload

---
+++ project_assadinos1/javadop.js
@@ -0,0 +1,10 @@
+const raw= document.getElementById("raw").innerHTML;
+
+
+
+function abba( ){
+    const son ={
+        img = document.querySelectorAll("img")
+        text = document.querySelectorAll("p")
+    }
+}
